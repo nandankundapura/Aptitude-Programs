f29f89e813d5c293c5ad40f36a509914ccc03ce2
nandankundapura <nandankundapura777@gmail.com>
2025-08-22 20:58:24 +0530

    Calculator sample file

---
+++ AverageCalculator.java
@@ -0,0 +1,16 @@
+package AptitudeProgram;
+
+public class AverageCalculator {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for (int num : numbers) {
+            sum += num;
+        }
+
+        double average = (double) sum / numbers.length;
+
+        System.out.println("Average = " + average);
+    }
+}
